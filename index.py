a5eb3730eacce84c60258f62aa4c68e3d51f65e3
Katherine Manning <katm201@gmail.com>
2018-03-15 19:54:11 -0500

    [feat] update size input message

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ def play_game():
 
     player_1 = input('Player 1, what\'s your name? ')
     player_2 = input('Player 2, what\'s your name? ')
-    size = input('How many rows/columns in your board? Minimum size is 3: ')
+    size = input('How many rows/columns in your board? Minimum size is 3, maximum size is 8: ')
 
     game = tic_tac_toe.TicTacToe(player_1, player_2, int(size))
     
